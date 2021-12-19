cec97b252cb7b6eb4a94f1f4ec748832d57e32b3
Paolo <paolo.monni345@gmail.com>
2021-12-19 12:20:33 +0100

    Remove old code

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -29,26 +29,3 @@ map('n', '<C-h>',       ':bprev<CR>', default_opts)
 map('n', '<F2>',	':Vista<CR>', default_opts)
 map('n', '<F5>',	':COQnow<CR>', default_opts)
 
--- """ Functions
--- function! Generate_comments()
---     let snippet = [
--- 	  \ '"""Returns a list containing :class:`bluepy.btle.Characteristic`',
---           \ 'objects for the peripheral. If no arguments are given, will return all',
---           \ 'characteristics. If startHnd and/or endHnd are given, the list is',
---           \ 'restricted to characteristics whose handles are within the given range.',
--- 	  \ '',
---           \ ':param startHnd: Start index, defaults to 1',
---           \ ':type startHnd: int, optional',
---           \ ':param endHnd: End index, defaults to 0xFFFF',
---           \ ':type endHnd: int, optional',
---           \ ':param uuids: a list of UUID strings, defaults to None',
---           \ ':type uuids: list, optional',
---           \ ':return: List of returned :class:`bluepy.btle.Characteristic` objects',
---           \ ':rtype: list',
---           \ '"""',]
---     call append(line('.'), snippet)
--- endfunction
---
--- """ FZF window
--- """ general
-
